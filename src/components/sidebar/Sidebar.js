a46ece0cf6cd6dec4871574ebe10ed79c2540a58
Maria Filatkina <mfilatkina97@gmail.com>
2022-04-22 11:07:43 +0200

    Create sidebar

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,4 +1,10 @@
 import "./sidebar.scss";
+import userIcon from "../../resources/icons/user.svg";
+import studyIcon from "../../resources/icons/study.svg";
+import timerIcon from "../../resources/icons/timer.svg";
+import logoutIcon from "../../resources/icons/logout.svg";
+import sunIcon from "../../resources/icons/sun.svg";
+import moonIcon from "../../resources/icons/moon.svg";
 
 function Sidebar() {
   return (
@@ -8,12 +14,35 @@ function Sidebar() {
       </div>
       <hr />
       <div className="sidebar__center">
-        <ul>
-          <li>item</li>
-          <li>item</li>
-        </ul>
+        <nav className="sidebar__menu">
+          <ul>
+            <li>
+              <img src={userIcon} alt="user" />
+              <span>Профиль</span>
+            </li>
+            <li>
+              <img src={studyIcon} alt="study" />
+              <span>Обучение</span>
+            </li>
+            <li>
+              <img src={timerIcon} alt="timer" />
+              <span>Таймер</span>
+            </li>
+            <li>
+              <img src={logoutIcon} alt="logout" />
+              <span>Выйти</span>
+            </li>
+          </ul>
+        </nav>
       </div>
-      <div className="sidebar__bottom">color options</div>
+      <div className="sidebar__bottom">
+        <label htmlFor="switch" className="switch">
+          <img src={sunIcon} alt="sun" />
+          <input type="checkbox" id="switch" className="visually-hidden" />
+          <span>Dark mode</span>
+          <img src={moonIcon} alt="moon" />
+        </label>
+      </div>
     </div>
   );
 }
