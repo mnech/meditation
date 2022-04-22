f8e563e456e3814b404eb9dd5c7b8fb8aca8137a
Maria Filatkina <mfilatkina97@gmail.com>
2022-04-22 16:01:58 +0200

    Add spinner

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,27 +1,38 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Profile from "../../pages/profile/Profile";
-import Login from "../../pages/login/Login";
-import LessonsList from "../../pages/lessonsList/LessonsList";
-import Lesson from "../../pages/lesson/Lesson";
-import Timer from "../../pages/timer/Timer";
+import Sidebar from "../sidebar/Sidebar";
+import Spinner from "../spinner/Spinner";
 
 import "../../style/style.scss";
+
+const Profile = lazy(() => import("../../pages/profile/Profile"));
+const Login = lazy(() => import("../../pages/login/Login"));
+const LessonsList = lazy(() => import("../../pages/lessonsList/LessonsList"));
+const Lesson = lazy(() => import("../../pages/lesson/Lesson"));
+const Timer = lazy(() => import("../../pages/timer/Timer"));
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/">
-          <Route index element={<Profile />} />
-          <Route path="login" element={<Login />} />
-          <Route path="lessons">
-            <Route index element={<LessonsList />} />
-            <Route path=":id" element={<Lesson />} />
-          </Route>
-          <Route path="timer" element={<Timer />} />
-        </Route>
-      </Routes>
+      <div className="app">
+        <Sidebar />
+        <main className="app__main">
+          <Suspense fallback={<Spinner className="spinner" />}>
+            <Routes>
+              <Route path="/">
+                <Route index element={<Profile />} />
+                <Route path="login" element={<Login />} />
+                <Route path="lessons">
+                  <Route index element={<LessonsList />} />
+                  <Route path=":id" element={<Lesson />} />
+                </Route>
+                <Route path="timer" element={<Timer />} />
+              </Route>
+            </Routes>
+          </Suspense>
+        </main>
+      </div>
     </BrowserRouter>
   );
 }
--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,13 +1,7 @@
-import Sidebar from "../../components/sidebar/Sidebar";
 import "./profile.scss";
 
 function Profile() {
-  return (
-    <div className="profile">
-      <Sidebar />
-      <div className="profile__container">container</div>
-    </div>
-  );
+  return <div className="profile">profile</div>;
 }
 
 export default Profile;
