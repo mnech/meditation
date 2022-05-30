25c93d7272530192844244e28c63d10110665e7e
Maria Filatkina <mfilatkina97@gmail.com>
2022-05-30 20:57:12 +0200

    fix upload user photo

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,99 +1,59 @@
 import { useEffect, useMemo, useState } from "react";
 
 import { uploadFile } from "../../services/firebase";
-import useFetch from "../../hooks/useFetch";
-import setContent from "../../utils/setContent";
 import defaultImage from "../../resources/img/camera_photo.png";
 import Spinner from "../../components/spinner/Spinner";
+import ErrorMessage from "../../components/errorMessage/ErrorMessage";
 import "./profile.scss";
 
 function Profile() {
-  const [file, setFile] = useState("");
-  const [perc, setPerc] = useState(null);
-
-  const uploadImage = () => {
-    console.log("upload");
-    if (!file) {
-      return;
-    }
-    console.log("upload 1");
-    const name = new Date().getTime() + file.name;
-    uploadFile(file, name, setPerc);
-  };
-
-  const { fetching, process, setProcess } = useFetch(() =>
-    // eslint-disable-next-line no-use-before-define
-    uploadImage().then(onDataLoaded),
-  );
-
-  console.log(process);
-
-  const onDataLoaded = (url) => {
-    console.log("data loaded");
-    if (url) {
-      setProcess("confirmed");
-    } else {
-      setProcess("error");
-    }
-  };
+  const [photo, setPhoto] = useState("");
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const userPhoto = useMemo(() => {
-    if (file) {
-      return URL.createObjectURL(file);
+    if (photo) {
+      return photo;
     }
     return defaultImage;
-  }, [file]);
+  }, [photo]);
 
   const changeFile = (e) => {
+    setError(false);
     if (e.target.files.length) {
-      setFile(e.target.files[0]);
+      const file = e.target.files[0];
+      const name = new Date().getTime() + file.name;
+      uploadFile(file, name, setLoading, setError, setPhoto);
     }
   };
 
   const imgContent = useMemo(() => {
-    if (perc !== null && perc < 100) {
+    if (error) {
+      return <ErrorMessage />;
+    }
+    if (loading) {
       return <Spinner />;
     }
     return <img src={userPhoto} alt="user" className="profile__img" />;
-  }, [perc, userPhoto]);
+  }, [error, loading, userPhoto]);
 
-  useEffect(() => {
-    // uploadImage();
-    if (file) {
-      fetching();
-    }
-  }, [file]);
-
-  const renderContent = () => {
-    return (
-      <div>
-        <div className="profile__left">
-          {imgContent}
-          <input
-            type="file"
-            accept=".jpg, .jpeg, .png"
-            className="profile__load"
-            onChange={changeFile}
-          />
-        </div>
-        <div className="profile__right">
-          <div className="profile__name">Имя: Василий</div>
-          <div className="profile__email">Почта: vasya@mail.ru</div>
-        </div>
+  return (
+    <div className="profile">
+      <div className="profile__left">
+        {imgContent}
+        <input
+          type="file"
+          accept=".jpg, .jpeg, .png"
+          className="profile__load"
+          onChange={changeFile}
+        />
       </div>
-    );
-  };
-
-  const elements = useMemo(() => {
-    console.log("elements");
-    return setContent(process, () => {
-      renderContent();
-      console.log("render");
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [process]);
-
-  return <div className="profile">{elements}</div>;
+      <div className="profile__right">
+        <div className="profile__name">Имя: Василий</div>
+        <div className="profile__email">Почта: vasya@mail.ru</div>
+      </div>
+    </div>
+  );
 }
 
 export default Profile;
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -139,33 +139,26 @@ export const saveMeditationTime = async (userId, seconds) => {
 
 // other
 
-export const uploadFile = (file, name, setPerc) => {
+export const uploadFile = (file, name, setLoading, setError, setPhoto) => {
+  setLoading(true);
   const storageRef = ref(storage, name);
   const uploadTask = uploadBytesResumable(storageRef, file);
 
   uploadTask.on(
     "state_changed",
     (snapshot) => {
-      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-      setPerc(progress);
-      // switch (snapshot.state) {
-      //   case "paused":
-      //     console.log("Upload is paused");
-      //     break;
-      //   case "running":
-      //     console.log("Upload is running");
-      //     break;
-      //   default:
-      //     break;
-      // }
+      // const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
     },
     () => {
       // error
-      return null;
+      setLoading(false);
+      setError(true);
     },
     () => {
+      // upload completed successfully, now we can get the URL
       getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-        return downloadURL;
+        setPhoto(downloadURL);
+        setLoading(false);
       });
     },
   );
